a1e5a753e7df44f4731bd1afe0582cca049582ba
Fang-WeiXiong <71875526+Fang-WeiXiong@users.noreply.github.com>
2021-05-17 18:52:54 +0800

    完成了登录页面

--- src/main.js
+++ src/main.js
@@ -2,8 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+import axios from 'axios'
+axios.defaults.baseURL = 'http://timemeetyou.com:8889/api/private/v1/'
 
 Vue.config.productionTip = false
+Vue.prototype.$http = axios
+
 
 new Vue({
   router,
--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,36 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
+const Login = () => import('../components/Login.vue')
+const Home = () => import('../components/Home.vue')
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+const routes = [{
+		path: '/',
+		redirect: '/login'
+	},
+	{
+		path: '/login',
+		component: Login
+	},
+	{
+		path: '/home',
+		component: Home
+	}
 ]
 
 const router = new VueRouter({
-  routes
+	routes
+})
+
+router.beforeEach((to, from, next) => {
+	// 访问登录页，放行
+	if (to.path === '/login') return next()
+	// 获取token
+	const tokenStr = window.sessionStorage.getItem('token')
+	// 没有token, 强制跳转到登录页
+	if (!tokenStr) return next('/login')
+	next()
 })
 
 export default router
